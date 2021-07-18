b48e93448ea76bbc5f61239c416de8f3f79eeb14
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-18 23:55:09 +0300

    jwt-strategy added

---
+++ src/utils/jwt-strategy.js
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy} from 'passport-jwt'
+import { use, authenticate } from 'passport'
+
+export const jwtSecret = process.env.JWT_SECRET
+
+if(typeof jwtSecret === 'undefined'){
+    throw new Error('Jwt secret is missing.')
+}
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwtSecret
+}
+
+const strategy = new Strategy(opts, (payload, done) => {
+    return done(null, {
+        id: payload.id,
+        role: payload.role
+    });
+})
+
+use(strategy);
+export const verifyUser = authenticate(strategy, { session: false })
