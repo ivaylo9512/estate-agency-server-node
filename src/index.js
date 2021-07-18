a036ff3558ed4ea46c7d356adbf967fee7436a2f
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-18 23:58:35 +0300

    load-env + files change

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import "reflect-metadata";
+import "./utils/load-env.js"
 import "@babel/polyfill";
 import { createConnection } from "typeorm";
 import ormConfig from './type-orm.js'
 import express from 'express';
 import multer from 'multer';
+import propertyRouter from './routes/property-routes.js'
+import PropertyService from "./services/property-service.js";
 
 const main = async() => {
     const app = express();
 
     const connection = await createConnection(ormConfig);
 
-    multer({ dest: 'src/pubil' })
-    
+    multer({ dest: 'src/pubilc' })
     app.use(express.static('src/public'));
-    app.use(fileUpload({
-        limits: { fileSize: 50 * 1024 * 1024 },
-      }));
+
+    app.use('/properties', propertyRouter)
 
     const port = process.env.PORT || 8098;
     app.listen(port, () => {
