e462e21ddb132cabaa06d3f1503490fb44dd8e26
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-20 23:37:36 +0300

    EntityNotFoundException added

---
+++ src/exceptions/enitity-not-found-exception.js
@@ -0,0 +1,13 @@
+export default class EntitiyNotFoundException extends Error{
+    constructor(message){
+        super(message)
+        Error.captureStackTrace(this, this.constructor);
+
+        this.name = this.constructor.name;
+        this.status = 404;
+    }
+
+    statusCode(){
+        return this.status;
+    }
+}
