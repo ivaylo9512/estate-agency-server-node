e1b65f865ada196b8490ec2262fac104fbd7a4bd
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-21 04:18:11 +0300

    jwt added

---
+++ src/authentication/jwt.js
@@ -0,0 +1,26 @@
+import { sign }  from 'jsonwebtoken';
+import { DEFAULT_REFRESH_EXPIRY, DEFAULT_JWT_EXPIRY } from '../utils/constants';
+
+export const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+export const jwtSecret = process.env.JWT_SECRET;
+export const refreshExpiry = eval(process.env.REFRESH_TOKEN_EXPIRY || DEFAULT_REFRESH_EXPIRY);
+const jwtExpiry = eval(process.env.JWT_EXPIRY || DEFAULT_JWT_EXPIRY);
+
+if(typeof jwtSecret === 'undefined' || typeof refreshSecret === 'undefined'){
+    throw new Error('Refresh or jwt secrets are missing.');
+}
+export const COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: true,
+    signed: true,
+    maxAge: refreshExpiry * 1000,
+    sameSite: "none",
+}
+export const getToken = (user) => sign(user, 
+    jwtSecret, {
+        expiresIn: jwtExpiry,
+})
+export const getRefreshToken = (user) => sign(user, 
+    refreshSecret, {
+        expiresIn: refreshExpiry
+})
