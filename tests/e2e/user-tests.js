1698dc7446249f6be13d06e3804eb01799f8c7e3
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-22 17:39:17 +0300

    more user e2e test

--- tests/e2e/user-tests.js
+++ tests/e2e/user-tests.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from './sequential.test';
 import { getToken } from '../../src/authentication/jwt';
 
-export const firstUser = {
+const firstUser = {
     username: 'testUser', 
     password: 'testUserPassword', 
     name: 'testName', 
@@ -10,7 +10,7 @@ export const firstUser = {
     location: 'test location',
     email: 'testEmail@gmail.com'
 }
-export const secondUser = {
+const secondUser = {
     username: 'testUser2', 
     password: 'testUserPassword2', 
     name: 'testName2', 
@@ -18,9 +18,25 @@ export const secondUser = {
     location: 'test location2',
     email: 'testEmail2@gmail.com'
 }
+export const updatedFirstUser = {
+    id: 1,
+    username: 'testUserUpdated', 
+    name: 'testNameUpdated', 
+    description: 'test description updated', 
+    location: 'test location updated',
+    email: 'testEmailUpdated@gmail.com'
+}
+export const updatedSecondUser = {
+    id: 2,
+    username: 'testUserUpdated2', 
+    name: 'testNameUpdated2', 
+    description: 'test descriptionUpdated2', 
+    location: 'test locationUpdated2',
+    email: 'testEmailUpdated2@gmail.com'
+}
 export const adminToken = 'Bearer ' + getToken({
     id: 3, 
-    admin: 'admin'
+    role: 'admin'
 })
 export let firstToken;
 export let secondToken;
@@ -55,13 +71,13 @@ const userTests = () => {
 
                 secondUser.id = res.body.id;
                 secondToken = 'Bearer ' + res.get('Authorization');
-                delete secondUser.password 
+                delete secondUser.password;
 
                 expect(res.body.id).toBe(2);
                 expect(res.body).toEqual(secondUser);
         })
 
-        it('should login user', async() => {
+        it('should login user with username', async() => {
             const res = await request(app)
                 .post('/users/login')
                 .set('Content-Type', 'Application/json')
@@ -72,6 +88,67 @@ const userTests = () => {
                 .expect(200);
 
                 expect(res.body).toEqual(secondUser);
+        })
+
+        it('should throw Unauthorized when login user with wrong password', async() => {
+            const res = await request(app)
+                .post('/users/login')
+                .set('Content-Type', 'Application/json')
+                .send({
+                    email: secondUser.email,
+                    password: 'wrongPassword'
+                })
+                .expect(401);
+
+                expect(res.text).toEqual('Incorrect username, pasword or email.');
+        })
+
+        it('should return firstUser when findById with id 1', async() => {
+            const res = await request(app)
+                .get('/users/findById/1')
+                .expect(200);
+
+                expect(res.body).toEqual(firstUser);
+        })
+
+        it('should throw EntityNotFound when findById with nonexistent id', async() => {
+            const res = await request(app)
+                .get('/users/findById/252')
+                .expect(404);
+
+                expect(res.text).toEqual('Could not find any entity of type "User" matching: {\n    "id": 252\n}');
+        })
+
+        it('should throw when updating user from another loggedUser that is not admin: role', async() => {
+            const res = await request(app)
+                .patch('/users/auth/update')
+                .set('Content-Type', 'Application/json')
+                .set('Authorization', secondToken)
+                .send(updatedFirstUser)
+                .expect(401);
+
+                expect(res.text).toEqual('Unauthorized.');
+        })
+
+        it('should update user when udating with same looged user id', async() => {
+            const res = await request(app)
+                .patch('/users/auth/update')
+                .set('Content-Type', 'Application/json')
+                .set('Authorization', firstToken)
+                .send(updatedFirstUser)
+                .expect(200);
+
+                expect(res.body).toEqual(updatedFirstUser);
+        })
+        it('should update user when udating with looged user with role: admin', async() => {
+            const res = await request(app)
+                .patch('/users/auth/update')
+                .set('Content-Type', 'Application/json')
+                .set('Authorization', adminToken)
+                .send(updatedSecondUser)
+                .expect(200);
+
+                expect(res.body).toEqual(updatedSecondUser);
         })
     }
 } 
