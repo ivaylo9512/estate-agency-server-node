e86ec1e6e38d07d4b4ba2ade0e49d5231ea3e2b7
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-24 22:21:33 +0300

    createValidation middleware added + old validations removed

--- src/validators/users-validator.js
+++ src/validators/users-validator.js
@@ -27,3 +27,52 @@ export const registerValidationRules = () => [
     check('location', 'You must provide a location.').notEmpty(),
     check('description', 'You must provide a description.').notEmpty()
 ]
+
+export const createValidationRules = () => [
+    check('users.*.email', 'Must be a valid email.').isEmail(),
+    check('users.*.password', 'Password must be between 10 and 22 characters').isLength({min:10, max: 22}),
+    check('users.*.username', 'Username must be between 8 and 20 characters').isLength({min:8, max: 20}), 
+    check('users.*.name', 'You must provide a name.').notEmpty(),
+    check('users.*.location', 'You must provide a location.').notEmpty(),
+    check('users.*.description', 'You must provide a description.').notEmpty()
+]
+export const createValidation = async(req, res, next) => {
+    const result = validationResult(req);
+
+    if(!result.isEmpty()){
+        const error = result.errors.reduce((errorObject, error) => {
+            const params = error.param.match(/[^\[\].]+/g);
+            const user = params[0] + params[1];
+            const paramName = params[2];
+
+            errorObject[user] = {
+                ...errorObject[user], 
+                [paramName]: error.msg
+            }
+
+            return errorObject;
+        }, {})
+
+        return res.status(422).send(error);
+    }
+
+    const error = await req.body.users.reduce(async(errorObject, user, i) => {
+        const {username, email} = user;
+        const foundUser = await req.userService.findByUsernameOrEmail(username, email);
+
+        const errors = await errorObject;
+        if(foundUser){
+            errors['user' + i] = {
+                username: 'User with given username or email already exists.'
+            }
+        }
+        
+        return errors;
+    }, {}) 
+
+    if(Object.keys(error).length > 0){
+        return res.status(422).send(error);
+    }
+
+    next();
+}
