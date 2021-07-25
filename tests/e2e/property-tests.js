a61ba209151d1a316a6c9db6563ed7dad837aab1
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-25 19:21:29 +0300

    more property tests

--- tests/e2e/property-tests.js
+++ tests/e2e/property-tests.js
@@ -6,8 +6,8 @@ const propertyTests = () => {
     const [fistProperty, secondProperty, thirdProperty, forthProperty] = Array.from({length: 4}, (v, i) => ({
         name: 'testProperty' + i,
         description: 'testProperty' + i,
-        price: 4000000 * i,
-        size: 12000 * i,
+        price: 4000000 * (i + 1),
+        size: 12000 * (i + 1),
         location: 'testLocation' + i
     }));
     
@@ -15,8 +15,8 @@ const propertyTests = () => {
         id: i + 1,
         name: 'testPropertyUpdated' + i,
         description: 'testPropertyUpdated' + i,
-        price: 4500000 * i,
-        size: 15000 * i,
+        price: 4500000 * (i + 1),
+        size: 15000 * (i + 1), 
         location: 'testLocationUpdated' + i
     }))
 
@@ -27,14 +27,12 @@ const propertyTests = () => {
             .set('Authorization', firstToken)
             .send(fistProperty)
 
-            const {id, owner} = res.body;
-
+            const { id, owner } = res.body;
             fistProperty.id = id;
             fistProperty.owner = owner;
-            updatedFirstProperty.owner = owner;
-
+            
             expect(owner.id).toBe(1);
-            expect(res.body.id).toBe(1);
+            expect(id).toBe(1);
             expect(res.body).toEqual(fistProperty);
     })
 
@@ -89,7 +87,7 @@ const propertyTests = () => {
             .send({ properties: [secondProperty, thirdProperty, forthProperty] })
             .expect(200);
 
-            const [{id: secondId, owner}, {id: thirdId}, {id: forthId}] = res.body;
+            const [{id: secondId}, {id: thirdId}, {id: forthId}] = res.body;
 
             secondProperty.id = secondId;
             thirdProperty.id = thirdId;
@@ -132,8 +130,27 @@ const propertyTests = () => {
             expect(res.body).toEqual(error);
     })
 
-    it('should get a property with id: 1', async () => {
+    it('should return 422 when creating properties with incorrect input', async () => {
+        const error = {
+            properties0: {
+                description: 'You must provide a description.', 
+                location: 'You must provide a location.', 
+                name: 'You must provide a name.', 
+                price: 'You must provide price as a number.', 
+                size: 'You must provide a size.'
+            }
+        }
+        const res = await request(app)
+            .post('/properties/auth/createMany')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', firstToken)
+            .send({ properties: [{price: 'text'}] })
+            .expect(422);
 
+            expect(res.body).toEqual(error);
+    })
+
+    it('should get a property with id: 1', async () => {
         const res = await request(app)
             .get('/properties/findById/1')
             .expect(200);
@@ -143,7 +160,6 @@ const propertyTests = () => {
 
     
     it('should return 404 when findById with nonexistent id', async () => {
-
         const res = await request(app)
             .get('/properties/findById/222')
             .expect(404);
@@ -152,14 +168,80 @@ const propertyTests = () => {
     })
 
     it('should update property', async () => {
-
         const res = await request(app)
             .patch('/properties/auth/update')
             .set('Content-Type', 'Application/json')
             .set('Authorization', firstToken)
             .send(updatedFirstProperty)
+            .expect(200);
+
+            updatedFirstProperty.owner = res.body.owner;
 
             expect(res.body).toEqual(updatedFirstProperty);
+    })
+
+    it('should return 401 when updating property from user with different id that is not admin', async () => {
+        const res = await request(app)
+            .patch('/properties/auth/update')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', secondToken)
+            .send(updatedFirstProperty)
+            .expect(401);
+
+            expect(res.text).toEqual('Unauthorized.');
+    })
+
+    it('should update property when updating from user with different id that is admin', async () => {
+        const res = await request(app)
+            .patch('/properties/auth/update')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', adminToken)
+            .send(updatedSecondProperty)
+            .expect(200);
+
+            updatedSecondProperty.owner = res.body.owner;
+
+            expect(res.body).toEqual(updatedSecondProperty);
+    })
+
+    it('should return 422 when updating with invalid inputs', async () => {
+        const error =  {
+            id: 'You must provide an id.', 
+            description: 'You must provide a description.', 
+            location: 'You must provide a location.', 
+            name: 'You must provide a name.', 
+            price: 'You must provide a price', 
+            size: 'You must provide a size.'
+        }
+
+        const res = await request(app)
+            .patch('/properties/auth/update')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', adminToken)
+            .send({})
+            .expect(422);
+
+            expect(res.body).toEqual(error);
+    })
+
+    it('should return 422 when updating with invalid inputs', async () => {
+        const error =  {
+            id: 'You must provide id as a whole number.',
+            description: 'You must provide a description.', 
+            location: 'You must provide a location.', 
+            name: 'You must provide a name.', 
+            price: 'You must provide price as a number.', 
+            size: 'You must provide a size.'
+        }
+
+        const res = await request(app)
+            .patch('/properties/auth/update')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', adminToken)
+            .send({id: 'text', price: 'text'})
+            .expect(422);
+
+            expect(res.body).toEqual(error);
     })
 
     it('should return 401 when deleting property wtihout token', async() => {
