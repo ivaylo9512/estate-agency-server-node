d3ed0f49eb410865cf031b8ac52cfce3123591e1
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-25 17:38:58 +0300

    property validator added

---
+++ src/validators/properties-validator.js
@@ -0,0 +1,41 @@
+import { check, validationResult } from 'express-validator';
+
+export const createValidationRules = [
+    check('size', 'You must provide a size.').notEmpty(),
+    check('price', 'You must provide a price').notEmpty().bail().isNumeric().withMessage('You must provide price as a number.'), 
+    check('name', 'You must provide a name.').notEmpty(),
+    check('location', 'You must provide a location.').notEmpty(),
+    check('description', 'You must provide a description.').notEmpty()
+]
+
+export const updateValidationRules = [
+    check('id', 'You must provide an id.').notEmpty().bail().isInt().withMessage('You must provide id as a whole number.'),
+    check('size', 'You must provide a size.').notEmpty(),
+    check('price', 'You must provide a price').notEmpty().bail().isNumeric().withMessage('You must provide price as a number.'), 
+    check('name', 'You must provide a name.').notEmpty(),
+    check('location', 'You must provide a location.').notEmpty(),
+    check('description', 'You must provide a description.').notEmpty()
+]
+
+export const validator = (req, res, next) => {
+    if(checkForInputErrors(req, res)){
+        return;
+    }
+
+    next()
+}
+
+const checkForInputErrors = (req, res) => {
+    const result = validationResult(req);
+
+    if(!result.isEmpty()){
+        const errors = result.errors.reduce((errorObject, error) => {
+            errorObject[error.param] = error.msg;
+            
+            return errorObject;
+        }, {})
+
+        res.status(422).send(errors);
+        return errors;
+    }
+}
